d81d9d279d5f30a5ffe521f85bf2c12d7ee3a7f8
Vishal Parmar <108256896+kevit-Vishal-Parmar@users.noreply.github.com>
2025-05-25 16:58:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // const appConfig = require("./src/config/app.config");
 const app = require("./src/app");
-// const databaseConnection = require("./src/database/db");
+// const databaseConnection = require("./src/database/db"); 
 
 databaseConnection.then(() => {
   app.listen(appConfig.PORT, () => {
